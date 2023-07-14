403b39ca871e3fb70a88e700396a60813a3b7292
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-07-14 21:39:56 +0100

    (bard.py) feat add fisrt slide

--- bard.py
+++ bard.py
@@ -9,9 +9,40 @@ from pptx.oxml.xmlchemy import OxmlElement
 from pptx.util import Inches
 from pptx.util import Pt
 
-# from pptx.enum.text import PP_BULLET_TYPE
+FIT_NONE = MSO_AUTO_SIZE.NONE
 
-# from pptx import Presentation
+ALIGN_TOP = MSO_ANCHOR.TOP
+ALIGN_CENTER = PP_ALIGN.CENTER
+ALIGN_LEFT = PP_ALIGN.LEFT
+
+BLUE_DARK1 = RGBColor(30, 4, 91)
+BLUE_DARK2 = RGBColor(0, 32, 96)
+RED = RGBColor(255, 0, 0)
+GREEN = RGBColor(0, 135, 0)
+BLACK = RGBColor(0, 0, 0)
+
+IMG_RIGHT = True
+IMG_BUTTOM = None
+
+
+def print_placeholder(presentation):
+    for i in range(99):
+        print(f"--------{i}--------")
+        master_layout = i
+        try:
+            slide = presentation.slides.add_slide(
+                presentation.slide_layouts[master_layout]
+            )
+            # for shape in slide.placeholders:
+            #     print("%d %s" % (shape.placeholder_format.idx, shape.name))
+            for shape in slide.shapes:
+                # print('%s' % shape.shape_type)
+                if shape.is_placeholder:
+                    phf = shape.placeholder_format
+                    print("%d, %s" % (phf.idx, phf.type))
+            print("_________________")
+        except Exception:
+            break
 
 
 def SubElement(parent, tagname, **kwargs):
@@ -52,8 +83,8 @@ def set_options(
     Y=None,
     color=None,
     bold=None,
-    size=None,
-    font=None,
+    font_size=None,
+    font_name=None,
     align_H=None,
     align_V=None,
     shrink=None,
@@ -70,16 +101,17 @@ def set_options(
         shape.top = X
     if Y is not None:
         shape.left = Y
-    if color is not None:
-        shape.text_frame.paragraphs[0].font.color.rgb = color
-    if bold is not None:
-        shape.text_frame.paragraphs[0].font.bold = bold
-    if size is not None:
-        shape.text_frame.paragraphs[0].font.size = Pt(size)
-    if font is not None:
-        shape.text_frame.paragraphs[0].font.name = font
-    if align_H is not None:
-        shape.text_frame.paragraphs[0].alignment = align_H
+    for paragraph in shape.text_frame.paragraphs:
+        if color is not None:
+            paragraph.font.color.rgb = color
+        if bold is not None:
+            paragraph.font.bold = bold
+        if font_size is not None:
+            paragraph.font.size = Pt(font_size)
+        if font_name is not None:
+            paragraph.font.name = font_name
+        if align_H is not None:
+            paragraph.alignment = align_H
     if align_V is not None:
         shape.text_frame.vertical_anchor = align_V
 
@@ -87,36 +119,52 @@ def set_options(
         shape.text_frame.auto_size = shrink
 
 
-def add_paragraph(text_box=None, text=None, symbol="●") -> None:
+def add_paragraph_bac(text_box=None, text=None, symbol="●") -> None:
     if text_box is None or text is None:
         exit("missing args in add_paragraph")
+    paragraph = text_box.font.size
     paragraph = text_box.add_paragraph()
     paragraph.text = f" {text}"
     makeParaBulletPointed(paragraph, symbol)
 
 
-def add_slide(master_layout, title=None, subtitle=None, symbol="●", texts=None) -> None:
-    presentation = pptx.Presentation()
+def add_paragraph(text_box=None, text=None, symbol="●") -> None:
+    if text_box is None or text is None:
+        exit("missing args in add_paragraph")
+    paragraph = text_box.add_paragraph()
+    paragraph.text = text
+
+
+def add_slide(
+    title=None,
+    subtitle=None,
+    symbol="●",
+    texts=None,
+    image_path=None,
+    image_position=None,
+) -> None:
+    # presentation = pptx.Presentation("template1.pptx")
+    presentation = pptx.Presentation("/tmp/t8.pptx")
+    # presentation = pptx.Presentation("candy.pptx")
+    # presentation = pptx.Presentation()
 
     presentation.slide_width = Inches(16)
     presentation.slide_height = Inches(9)
 
-    slide = presentation.slides.add_slide(presentation.slide_layouts[master_layout])
+    # print_placeholder(presentation)
+    slide = presentation.slides.add_slide(presentation.slide_layouts[0])
+
+    ##1 text_box = slide.placeholders[2]
+    ##1 text_frame = text_box.text_frame
+    ##1
+    ##1 paragraph1 = text_frame.add_paragraph()
+    ##1 paragraph1.text = "AAA"
+    ##1 paragraph2 = text_frame.add_paragraph()
+    ##1 paragraph2.text = "BBB"
+    ##1 print("fin")
+    # return presentation
+
     # TODO: if placeholder = ... do ... elif do ...elif do...
-
-    FIT_NONE = MSO_AUTO_SIZE.NONE
-
-    ALIGN_TOP = MSO_ANCHOR.TOP
-    ALIGN_CENTER = PP_ALIGN.CENTER
-    ALIGN_LEFT = PP_ALIGN.LEFT
-
-    BLUE_DARK1 = RGBColor(30, 4, 91)
-    BLUE_DARK2 = RGBColor(0, 32, 96)
-    RED = RGBColor(255, 0, 0)
-    GREEN = RGBColor(0, 135, 0)
-
-    for shape in slide.placeholders:
-        print("%d %s" % (shape.placeholder_format.idx, shape.name))
 
     if title is not None:
         title_shape = slide.shapes.title
@@ -131,8 +179,8 @@ def add_slide(master_layout, title=None, subtitle=None, symbol="●", texts=None
             Y=Inches(0),
             color=RED,
             bold=True,
-            size=36,
-            font="Arial",
+            font_size=36,
+            font_name="Arial",
             align_H=ALIGN_CENTER,
             align_V=ALIGN_TOP,
             shrink=FIT_NONE,
@@ -150,37 +198,53 @@ def add_slide(master_layout, title=None, subtitle=None, symbol="●", texts=None
             Y=Inches(1),
             color=GREEN,
             bold=True,
-            size=36,
-            font="Monotype Corsiva",
+            font_size=36,
+            font_name="Monotype Corsiva",
             align_H=ALIGN_LEFT,
             align_V=ALIGN_TOP,
             shrink=FIT_NONE,
         )
     if texts is not None:
-        text_box = slide.placeholders[4]
+        text_box = slide.placeholders[2]
         text_frame = text_box.text_frame
         for text in texts:
             add_paragraph(text_frame, symbol=symbol, text=text)
+
+        if image_position is IMG_RIGHT:
+            img_width = Inches(4)
+        else:
+            img_width = Inches(0)
 
         set_options(
             text_box,
             text=None,
             upper=None,
-            width=Inches(15),
+            width=Inches(15) - img_width,
             height=None,
-            X=Inches(2.25),
+            X=Inches(2.50),
             Y=Inches(1),
-            color=BLUE_DARK2,
+            color=BLACK,
             bold=False,
-            size=30,
-            font="Arial",
+            font_size=24,
+            font_name="Arial",
             align_H=ALIGN_LEFT,
             align_V=ALIGN_TOP,
             shrink=FIT_NONE,
         )
 
-    # if image is not None:
-    #     image_shape = slide.shapes.add_picture(image, 0, 0, 100, 100)
+    if image_path is not None:
+        shapes = slide.shapes
+        if image_position is IMG_RIGHT:
+            left = Inches(12)
+            top = Inches(3)
+            width = Inches(3.90)
+        else:
+            left = Inches(6.5)
+            top = Inches(5.30)
+            width = None
+        height = Inches(3)
+
+        shapes.add_picture("img.png", left, top, width=width, height=height)
 
     return presentation
 
@@ -188,56 +252,19 @@ def add_slide(master_layout, title=None, subtitle=None, symbol="●", texts=None
 # 1,3,7 and ... has bullet
 if __name__ == "__main__":
     presentation = add_slide(
-        master_layout=4,
         title="Introduction",
         subtitle="Définitions",
         symbol="-",
         texts=[
-            "Une source radioactive est une quantité connue d'un radionucléide",
-            "qui émet un rayonnement ionisant.",
+            "Une source radioactive est une quantité connue d'un radionucléide qui émet un rayonnement ionisant.",
+            "Un rayonnement ionisant est un rayonnement électromagnétique ou corpusculaire capable de produire directement ou indirectement des ions lors de son passage à travers la matière. ",
+            "CCC",
         ],
-        # image="img.png",
+        image_path="img.png",
+        image_position=IMG_BUTTOM, # IMG_RIGHT
     )
+
     if presentation is not None:
         presentation.save("new_slide.pptx")
     else:
         exit("Presentation not saved")
-
-
-###------------------------------------------------------------
-# i: 0
-#       0 Title 1
-#       1 Subtitle 2
-# i: 1
-#       0 Title 1
-#       1 Content Placeholder 2
-# i: 2
-#       0 Title 1
-#       1 Text Placeholder 2
-# i: 3
-#       0 Title 1
-#       1 Content Placeholder 2
-#       2 Content Placeholder 3
-# i: 4
-#       0 Title 1
-#       1 Text Placeholder 2
-#       2 Content Placeholder 3
-#       3 Text Placeholder 4
-#       4 Content Placeholder 5
-# i: 5
-#       0 Title 1
-# i: 6
-# i: 7
-#       0 Title 1
-#       1 Content Placeholder 2
-#       2 Text Placeholder 3
-# i: 8
-#       0 Title 1
-#       1 Picture Placeholder 2
-#       2 Text Placeholder 3
-# i: 9
-#       0 Title 1
-#       1 Vertical Text Placeholder 2
-# X: 10
-#       0 Vertical Title 1
-#       1 Vertical Text Placeholder 2
